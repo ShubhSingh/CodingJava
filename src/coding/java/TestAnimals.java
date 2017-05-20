402aae059a3c449dcbb8c6a175ff7cff2d42e2f5
Shubham Singh <Shubham Singh>
2017-05-20 11:01:00 +0530

    Updated examples

--- src/coding/java/TestAnimals.java
+++ src/coding/java/TestAnimals.java
@@ -1,3 +1,12 @@
+package coding.java;
+/* When eat() in Horse class is public it prints:
+ * Generic Animal Eating Generically
+ * Horse eating hay, oats and Horse treats
+ * 
+ * When eat() in Horse class is private it prints:
+ * Generic Animal Eating Generically
+ * Generic Animal Eating Generically
+ *  */
 public class TestAnimals {
 	public static void main(String[] args) {
 		Animal a = new Animal();
@@ -12,7 +21,7 @@ class Animal {
 	}
 }
 class Horse extends Animal {
-	private void eat() {
+	 private void eat() {
 		System.out.println("Horse eating hay, oats and Horse treats");
 	}
 }
