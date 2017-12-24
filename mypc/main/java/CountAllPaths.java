760ac24eec9c91297ca2b2a67f5aa6996d37256b
ShubhSingh <send2shubham@gmail.com>
2017-12-24 09:00:18 +0530

    Fixed example

--- mypc/main/java/CountAllPaths.java
+++ mypc/main/java/CountAllPaths.java
@@ -4,72 +4,69 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-//This class represents a directed graph using adjacency list representation
-public class Graph {
+/**
+ * This class represents a directed graph using adjacency list representation
+ */
+public class CountAllPaths {
 	private int V; // No. of vertices
 
 	// Array of lists for Adjacency List Representation
 	private LinkedList<Integer> adj[];
 
 	// Constructor
-	Graph(int v) {
+	CountAllPaths(int v) {
 		V = v;
 		adj = new LinkedList[v];
 		for (int i = 0; i < v; ++i)
-			adj[i] = new LinkedList();
+			adj[i] = new LinkedList<>();
 	}
 
-	// Function to add an edge into the graph
+	// Method to add an edge into the graph
 	void addEdge(int v, int w) {
 		adj[v].add(w); // Add w to v's list.
 	}
 
-	/*
-	 * A recursive function to print all paths from 'u' to 'd'. visited[] keeps
-	 * track of vertices in current path. path[] stores actual vertices and
-	 * path_index is current index in path[]
+	/**
+	 * A recursive method to count all paths from 'u' to 'd'.
 	 */
-	void countPathsUtil(int u, int d, boolean visited[], int pathCount) {
+	int countPathsUtil(int u, int d, boolean visited[], int pathCount) {
 		// Mark the current node as visited and print it
 		visited[u] = true;
 
 		// If current vertex is same as destination, then increment count
 		if (u == d) {
 			pathCount++;
-			System.out.println(pathCount);
 		}
 			
-
 		// Recur for all the vertices adjacent to this vertex
-		//else {
+		else {
 			Iterator<Integer> i = adj[u].listIterator();
 			while (i.hasNext()) {
 				int n = i.next();
 				if (!visited[n]) {
-					System.out.print(pathCount+" :");
-					countPathsUtil(n, d, visited, pathCount);
+					pathCount = countPathsUtil(n, d, visited, pathCount);
 				}
 			}
-		//}
+		}
 
 		visited[u] = false;
+		return pathCount;
 	}
 
-	// Returns count of paths from 's' to 'd'
+	/* Returns count of paths from 's' to 'd'*/
 	int countPaths(int s, int d) {
 		// Mark all the vertices as not visited
 		boolean visited[] = new boolean[V];
 		Arrays.fill(visited, false);
 
-		// Call the recursive helper function to print
-		// all paths
+		// Call the recursive method to count all paths
 		int pathCount = 0;
-		countPathsUtil(s, d, visited, pathCount);
+		pathCount = countPathsUtil(s, d, visited, pathCount);
 		return pathCount;
 	}
 
 	public static void main(String args[]) {
-		Graph g = new Graph(4);
+		CountAllPaths g = new CountAllPaths(4);
 		g.addEdge(0, 1);
 		g.addEdge(0, 2);
 		g.addEdge(0, 3);
@@ -78,7 +75,6 @@ public class Graph {
 		g.addEdge(1, 3);
 
 		int s = 2, d = 3;
-		System.out.println("Total paths:");
 		System.out.println(g.countPaths(s, d));
 	}
 }
